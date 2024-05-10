3c7f7aa49d476736379594a81d8d42072bea610b
stpv22 <167645243+stpv221@users.noreply.github.com>
2024-05-10 12:21:16 -0700

    Create eagos.is-an.app.js

---
+++ domains/eagos.is-an.app.js
@@ -0,0 +1,14 @@
+addSubDomain({
+  description: 'EagOS - A WebOS based off of the windows 12 concept',
+  domain: 'is-an.app',
+  subdomain: 'eagos',
+  owner: {
+    repo: 'https://github.com/stpv221/EagOS',
+    email: 'oswaldfink12@gmail.com',
+  },
+  record: {
+    CNAME: 'stpv221.github.io/EagOS/
+',
+  },
+  proxy: false,
+})
